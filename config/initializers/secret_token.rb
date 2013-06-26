8b59c124b4a4cf69d3dbdfac361a23c5f68b589c
bry4n <bryann83@gmail.com>
2013-06-25 19:59:52 -0500

    Update app to rails 4.0

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Gitscm::Application.config.secret_token = '7578dada6e7d1904ecd87c0ac3ceffd14dbafa72a168be1772c4183db2d197b0f3fa330e8e9ea3288a4f59b08c5b8742984320fdac54a51a0986c59b4af512af'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitscm::Application.config.secret_key_base = '8ed81c097b08db2f4bd9c7e247cb5f94c072114f4873b950b41b84e7838315e7ce655cffebf8cccf493ac31e84928772a66b08fb66f5cc4afccb925136dd7f58'
